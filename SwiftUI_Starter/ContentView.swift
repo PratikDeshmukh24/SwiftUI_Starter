24bc73c9fb5e2e66533f28e66424fc748f105cb9
Deshmukh, Pratik Prabhakar <pratideshmukh@deloitte.com>
2020-07-13 17:27:48 +0530

    Initial Commit

---
+++ SwiftUI_Starter/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI_Starter
+//
+//  Created by Deshmukh, Pratik Prabhakar on 13/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Starter/SwiftUI_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StarterApp.swift
+//  SwiftUI_Starter
+//
+//  Created by Deshmukh, Pratik Prabhakar on 13/07/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
